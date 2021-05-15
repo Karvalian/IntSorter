8bb74f7200c66cbb2dc80c36d80048132e9b8395
Karvalian <obaid8197@gmail.com>
2021-05-15 09:31:45 -0700

    Array Sorter

---
+++ sol.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int t = 100;
+	vector<int> cool;
+	for(int i = 0; i<t; i++){
+		int n;
+		cin >> n;
+		cool.push_back(n);
+		sort(cool.begin(), cool.end());
+	}
+	for(int a : cool){
+		cout << a << endl;
+	}
+}
